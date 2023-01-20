6f7e1411410ef4809563ec4c1849d9d36a8a4bde
unknown <AliceLiu1218>
2023-01-19 23:59:25 -0500

    Add README file and ordered the javascript in where the password should be created

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # challenge3
+
+User inputs for password length is limited between 8-128 characters.
+
+textUpdated1, textUpdated2, textUpdated3, textUpdated4 traced down the string by user selection on whether they want to include any uppercase/lowercase/number/special character through slice string method.
+
+Up th here, the passwordlength and textUpdated4 have saved all the information needed.
+
+generatePassword function do the job of selecting the random password through math.random() built-in function from textUpdated4 string with PasswordLength length.
+
+document.querySelector("#password") interact with the attribute id="password"
+
+generateBtn.addEventListener("click", writePassword) add an eventlister when the generate button was clicked
--- script.js
+++ script.js
@@ -6,14 +6,13 @@ function generatePassword(PasswordLength,textUpdated4){
     var randomNumber = Math.floor(Math.random() * textUpdated4.length);
     password += textUpdated4.substring(randomNumber, randomNumber +1);
   }
+  console.log(password);
   return password;
 }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  
   //Grab value of password length from prompt window
   var UserInputPWLen = prompt("Please select for password length (Hint: Any value from 8 to 128 characters)");
   // Make sure the length is within the accpted range
@@ -68,10 +67,11 @@ function writePassword() {
   var UserInputSpecial = prompt("Do you want to include any special charchter? Type 'YES' OR 'NO'(casesensitive)");
   if (UserInputSpecial === "YES"){
     var textUpdated4 = textUpdated3; //FINAL TEXT
-    console.log(textUpdated4)
+    console.log(textUpdated4);
   }else if (UserInputSpecial === "NO"){
     if (count != 0){
       textUpdated4 = textUpdated3.replace(text.slice(62, 72),""); //FINAL TEXT
+      console.log(textUpdated4);
     }else{
       alert("You must contain the sepcial characters, otherwise the secure password wiil not be generated. Start Over required")
       return;
@@ -83,7 +83,7 @@ function writePassword() {
 
   var passwordGen = generatePassword(PasswordLength,textUpdated4);
   
-  var passwordText = document.querySelector("textarea");
+  var passwordText = document.querySelector("#password");
   passwordText.textContent = passwordGen;
 
 }
